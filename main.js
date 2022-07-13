4f38838e34b6479d0246dcf5dc52e0a9783df504
zoarno <97721931+zoarno@users.noreply.github.com>
2022-07-13 15:37:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 "use strict"
 
-// TODO: Set up how many balls we want
 let ballCount = 50; 
-// TODO: Set up x and y position variable to equal an empty array
-//TODO: Set up speed, size and color variable to equal an empty array
 let x = [];
 let y = [];
 let xSpeed = [];
@@ -13,74 +10,46 @@ let r = [];
 let g = [];
 let b = [];
 
-function setup() {
+function setup() 
+{
   createCanvas(windowWidth, windowHeight);
-
-  // TODO: Create a for loop that iterates through i until it reaches the ball count value
   for (let i = 0; i < ballCount; i++)
   {
-    // Inside the for loop:
-    // TODO: Set x and y position to be the center
-     // TODO: Set the speeds to be random
-
-    // TODO: Set the size to be random
-
-    // TODO: Set the colors to be random
-    x[i] = width / 2; 
-    y[i] = height / 2;
-    xSpeed[i] = random(-5, 5);
+    x[i] = width / 2;           //Set x position to be the center
+    y[i] = height / 2;          //Set y position to be the center 
+    xSpeed[i] = random(-5, 5);  //Set speed of x and y to be random
     ySpeed[i] = random(5, -5);
-    size[i] = random(10, 50);
-    r[i] = random(0, 256);
+    size[i] = random(10, 50); Set the ball size to be random
+    r[i] = random(0, 256); //Set colors to randomly generate 
     g[i] = random(0, 256);
     b[i] = random(0, 256);
-}
+  }
 }
 
-function draw() {
-  background(0, 50);
-  
-  // TODO: Iterate through a new for loop to change the properties in order to animate the balls
-for (let i = 0; i < ballCount; i++)
+function draw() 
 {
+  background(0, 50); //set background color
 
-  x[i] += xSpeed[i];
-  y[i] += ySpeed[i];
+    for (let i = 0; i < ballCount; i++)
+    {
+       x[i] += xSpeed[i];
+       y[i] += ySpeed[i];
 
-  if (x[i] < 0 || x[i] > width) 
-  {
-    xSpeed[i] *= -1; //reverse speed
+        if (x[i] < 0 || x[i] > width)  //If ball touches left or right sides, reverse the direction
+        {
+          xSpeed[i] *= -1; 
+        }
+
+        if (y[i] < 0 || y[i] > height) 
+        {
+        ySpeed[i] *= -1; ///If ball touches top or bottom sides, reverse the direction
+        }
+      
+      fill(r[i], g[i], b[i]); //Fills the r, g, b arrays with random color values 
+
+      noStroke(ellipse(x[i], y[i], size[i], size[i]));
   }
-
-   if (y[i] < 0 || y[i] > height) 
-  {
-    ySpeed[i] *= -1; //reverse speed
-  }
-
-  fill(r[i], g[i], b[i]);
-
-  noStroke(ellipse(x[i], y[i], size[i], size[i]));
 }
-
-}
-    // Inside the for loop:
-    // TODO: Increment speed for x position
-
-    // TODO: Increment speed for y position
-
-    
-    // TODO: Reverse x direction if ball hits left or right sides
-
-
-    // TODO: Reverse y direction if ball hits top or bottom sides
-
-
-    // TODO: Set random R, G, B values
-
-
-    // TODO: Style to have no strokes
-
-
-    // TODO: Draw the bouncing balls
+   
 
 
